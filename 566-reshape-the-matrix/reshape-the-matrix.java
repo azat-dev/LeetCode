8ee1bcb93b6c3841ecb6b0fdfe1a290a312840e7
Azat <kaumov.a.r@gmail.com>
2024-03-20 00:02:57 +0300

    Time: 1 ms (75.95%) | Memory: 45.6 MB (15.90%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        
+        final var numberOfRows = mat.length;
+        final var numberOfCols = mat[0].length;
+
+        final var matrixSize1 = numberOfRows * numberOfCols;
+        final var matrixSize2 = r * c;
+        
+        if (matrixSize1 != matrixSize2) {
+            return mat;
+        }
+
+        final var newMatrix = new int[r][c];
+
+        for (int rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+            for (int colIndex = 0; colIndex < numberOfCols; colIndex++) {
+                
+                final var itemIndex = rowIndex * numberOfCols + colIndex;
+                final var mappedRow = itemIndex / c;
+                final var mappedCol = itemIndex % c;
+
+                newMatrix[mappedRow][mappedCol] = mat[rowIndex][colIndex];
+            }
+        }
+
+        return newMatrix;
+    }
+}

39721edee2bf39875a04cadd81461c6c241fc9ae
Azat <kaumov.a.r@gmail.com>
2024-03-20 21:10:09 +0300

    Time: 41 ms (34.81%) | Memory: 16.7 MB (45.06%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -12,8 +12,6 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
         number_of_strings = len(strs)
-
-        # O(n)
         base_string = strs[0]
 
         for ch_index in range(len(base_string)):
@@ -23,6 +21,6 @@ class Solution:
                 str2 = strs[str_index]
 
                 if ch_index == len(str2) or str2[ch_index] != ch:
-                    return base_string[:ch_index] if ch_index > 0 else ""
+                    return base_string[:ch_index]
 
         return base_string

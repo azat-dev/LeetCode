7720d498bc433fa1140bad16c8f0d88686588066
Azat <kaumov.a.r@gmail.com>
2024-03-20 13:01:25 +0300

    Time: 2 ms (100.00%) | Memory: 44.7 MB (40.68%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,15 +1,26 @@
 class Solution {
+    final static int romanToDigit(char roman) {
+        switch (roman) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                throw new RuntimeException("Wrong value");
+        }
+    }
+    
     public int romanToInt(String s) {
-
-        Map<Character, Integer> digitsByRoman = Map.of(
-            'I', 1,
-            'V', 5,
-            'X', 10,
-            'L', 50,
-            'C', 100,
-            'D', 500,
-            'M', 1000
-        );
 
         var result = 0;
         var lastMaxDigit = -1;
@@ -17,7 +28,7 @@ class Solution {
         for (int i = s.length() - 1; i >= 0; i--) {
 
             final var roman = s.charAt(i);
-            final var digit = digitsByRoman.get(roman);
+            final var digit = romanToDigit(roman);
 
             if (digit >= lastMaxDigit) {
                 result += digit;

8ef6c3cb770186dd8535359f75acc8cd5f5f766b
Azat <kaumov.a.r@gmail.com>
2024-03-21 12:39:09 +0300

    Time: 5 ms (52.23%) | Memory: 45.6 MB (59.28%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,14 +1,18 @@
 class Solution {
+
     public int islandPerimeter(int[][] grid) {
         
         final var gridHeight = grid.length;
 
         var perimeter = 0;
+        var lastRowIndexWithIsland = -1;
 
         for (int row_index = 0; row_index < gridHeight; row_index++) {
 
             final var row = grid[row_index];
             final var gridWidth = row.length;
+            var bottomEdgesCount = 0;
+            var islandsCountInRow = 0;
 
             for (int col_index = 0; col_index < gridWidth; col_index++) {
 
@@ -16,6 +20,8 @@ class Solution {
                     continue;
                 }
                 
+                islandsCountInRow++;
+
                 final var hasTopEdge = (row_index == 0) || (grid[row_index - 1][col_index] == 0);
                 if (hasTopEdge) {
                     perimeter += 1;
@@ -34,7 +40,12 @@ class Solution {
                 final var hasBottomEdge = (row_index == gridHeight - 1) || (grid[row_index + 1][col_index] == 0);
                 if (hasBottomEdge) {
                     perimeter += 1;
+                    bottomEdgesCount++;
                 }
+            }
+
+            if (islandsCountInRow > 0 && islandsCountInRow == bottomEdgesCount) {
+                return perimeter;
             }
         }
 

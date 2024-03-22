8ff6f918ed38e1d079b74c0b6c0a0376e6069cec
Azat <kaumov.a.r@gmail.com>
2024-03-22 12:31:26 +0300

    Time: 15 ms (98.59%) | Memory: 15.7 MB (87.45%) - LeetSync

--- 1-two-sum/two-sum.swift
+++ 1-two-sum/two-sum.swift
@@ -1,31 +1,18 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         
-        guard nums.count > 1 else {
-            return []
-        }
-
-        var indexesByValues = [Int: Int]()
-
-        for index in 0..<nums.count {
-
-            let value = nums[index]
-            indexesByValues[value] = index
-        }
+        var indexesByNums = Dictionary<Int, Int>()
 
         for i in 0..<nums.count {
             
-            let a = nums[i]
-            let b = target - a
+            let currentValue = nums[i]
+            let complimentaryNumber = target - currentValue
 
-            guard 
-                let j = indexesByValues[b],
-                i != j
-            else {
-                continue
+            if let indexOfComplimentaryNumber = indexesByNums[complimentaryNumber] {
+                return [i, indexOfComplimentaryNumber]
             }
 
-            return [i, j]
+            indexesByNums[currentValue] = i
         }
 
         return []

9a91b2de468cfb44c1dd3885f1aa8c22b961742c
Azat <kaumov.a.r@gmail.com>
2024-03-27 12:16:37 +0300

    Time: 44 ms (6.24%) | Memory: 16.7 MB (9.70%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -2,8 +2,9 @@ class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         jewels_set = { ch for ch in jewels }
         count = 0
-        for ch in jewels_set:
-            count += stones.count(ch)
+        for ch in stones:
+            if ch in jewels_set:
+                count += 1
         
         return count
                 

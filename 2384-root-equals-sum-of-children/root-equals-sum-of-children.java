3f65c3d5fe9f26b5b1e3efe2668df7badd053b9d
Azat <kaumov.a.r@gmail.com>
2024-03-27 12:27:47 +0300

    Time: 0 ms (100.00%) | Memory: 41.2 MB (37.76%) - LeetSync

---
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        final var leftValue = root.left == null ? 0 : root.left.val;
+        final var rightValue = root.right == null ? 0 : root.right.val;
+        return root.val == leftValue + rightValue;
+    }
+}

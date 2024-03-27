7e2aae4fdc70778d3429cfd461dc43a6ea7cb1f1
Azat <kaumov.a.r@gmail.com>
2024-03-27 12:04:46 +0300

    Time: 6 ms (10.69%) | Memory: 42.9 MB (78.02%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,33 +1,27 @@
 class Solution {
 
-    // O(n) + O(m)
+    // O(n * m)
     public int[] intersection(int[] nums1, int[] nums2) {
 
-        if (nums1.length < nums2.length) {
-            final var temp = nums1;
-            nums1 = nums2;
-            nums2 = temp;
-        }
-
-        final var values2 = new HashSet<Integer>();
-
-        // O(m)
-        for (int i = 0; i < nums2.length; i++) {
-            values2.add(nums2[i]);
-        }
-
-        final var intersectedValues = new HashSet<Integer>();
+        var intersectedValues = new HashSet<Integer>();
 
         // O(n)
         for (int i = 0; i < nums1.length; i++) {
 
             final var currentValue = nums1[i];
             
-            if (!values2.contains(currentValue)) {
+            if (intersectedValues.contains(currentValue)) {
                 continue;
             }
 
-            intersectedValues.add(currentValue);
+            // O(m)
+            for (int j = 0; j < nums2.length; j++) {
+                final var value = nums2[j];
+
+                if (value == currentValue) {
+                    intersectedValues.add(currentValue);
+                }
+            }
         }
 
         final var result = new int[intersectedValues.size()];

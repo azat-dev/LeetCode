e60beceb17c8f1c39fc2f4ca4964a6157f79df42
Azat <kaumov.a.r@gmail.com>
2024-03-27 12:27:37 +0300

    Time: 31 ms (5.25%) | Memory: 62.4 MB (68.25%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,32 +1,30 @@
-import java.util.Arrays;
-
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
 
         Integer prevValue = null;
 
-        for (int i = 0, j = 0; i < nums1.length && j < nums2.length; ) {
+        for (int i = 0; i < nums1.length; i++) {
 
             final var currentValue = nums1[i];
 
             if (prevValue != null && prevValue == currentValue) {
-                i++;
                 continue;
             }
 
-            final var secondValue = nums2[j];
+            for (int j = 0; j < nums2.length; j++) {
 
-            if (secondValue < currentValue) {
-                j++;
-                continue;
-            }
+                final var secondValue = nums2[j];
 
-            if (secondValue == currentValue) {
+                if (secondValue > currentValue) {
+                    break;
+                }
+
+                if (currentValue != secondValue) {
+                    continue;
+                }
+
                 return currentValue;
             }
-
-            prevValue = currentValue;
-            i++;
         }
 
         return -1;

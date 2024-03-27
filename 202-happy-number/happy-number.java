5fe0413a527d6d2f045758ec28750d643eb4381b
Azat <kaumov.a.r@gmail.com>
2024-03-27 11:59:59 +0300

    Time: 2 ms (20.08%) | Memory: 41.2 MB (11.51%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,36 +1,29 @@
 class Solution {
-
-    private static int calcSum(int n) {
-        
-        int sum = 0;
-
-        while (n > 0) {
-            final var digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
-        }
-
-        return sum;
-    }
-
-    // Time Complexity: O(n)
-    // Space Complexity: O(1)
     public boolean isHappy(int n) {
-
-        int slowSum = n;
-        int fastSum = n;
-
-        do {
-
-            slowSum = calcSum(slowSum);
-            fastSum = calcSum(calcSum(fastSum));
-
-            if (slowSum == 1 || fastSum == 1) {
+        
+        var seen = new HashSet<Integer>();
+        
+        while (true) {
+            
+            if (seen.contains(n)) {
+                return false;
+            }
+            
+            seen.add(n);
+            
+            int sum = 0;
+            
+            while (n > 0) {
+                final var digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            
+            if (sum == 1) {
                 return true;
             }
-
-        } while (slowSum != fastSum);
-
-        return false;
+            
+            n = sum;
+        }
     }
 }

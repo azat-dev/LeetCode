d9f0754987a40fdd536673da03698b19efd4c927
Azat <kaumov.a.r@gmail.com>
2024-03-27 12:21:52 +0300

    Time: 2 ms (94.41%) | Memory: 42.8 MB (52.69%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -3,9 +3,9 @@ class Solution {
         
         for (int i = 0; i < arr.length; i++) {
             
-            for (int j = i + 1; j < arr.length; j++) {
+            for (int j = 0; j < arr.length; j++) {
                 
-                if (arr[i] == arr[j] * 2 || arr[i] * 2 == arr[j]) {
+                if (i != j && arr[i] == arr[j] * 2) {
                     return true;
                 }
             }

12108cd4d8b01cee3fc9c7f158dae60476f9ee36
Azat <kaumov.a.r@gmail.com>
2024-03-27 12:23:08 +0300

    Time: 1 ms (97.92%) | Memory: 45.2 MB (58.51%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        final var result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[nums[i]];
+        }
+
+        return result;
+    }
+}

46b9815e97971ef11e30e2e8a54f7897f18386ef
Azat <kaumov.a.r@gmail.com>
2024-03-27 12:22:11 +0300

    Time: 20 ms (6.50%) | Memory: 14 MB (100.00%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func runningSum(_ nums: [Int]) -> [Int] {
+        
+        var prevSum = 0
+        var results = [Int]()
+        
+        results.reserveCapacity(nums.count)
+
+        for num in nums {
+
+            prevSum += num
+            results.append(prevSum)
+        }
+
+        return results
+    }
+}

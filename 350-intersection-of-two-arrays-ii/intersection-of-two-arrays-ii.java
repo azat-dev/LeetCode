c64f5d6a087858d2f8867560b49c732ecf310d6b
Azat <kaumov.a.r@gmail.com>
2024-03-27 12:10:52 +0300

    Time: 5 ms (20.80%) | Memory: 43.7 MB (12.87%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+        final var map = new HashMap<Integer, Integer>(1000);
+        
+        // O(n1)
+        for(var x : nums1) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+        
+        final var list = new LinkedList<Integer>();
+        
+        // O(n2)
+        for (var x : nums2) {
+            
+            final var n = map.getOrDefault(x, 0);
+            
+            if (n == 0) {
+                continue;
+            }
+            
+            list.add(x);
+            map.put(x, n - 1);
+        }
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}

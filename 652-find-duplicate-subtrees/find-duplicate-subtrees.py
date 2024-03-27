7d4ba4e016fdf18cf147efa92c26a07eaea11a35
Azat <kaumov.a.r@gmail.com>
2024-03-27 12:14:46 +0300

    Time: 61 ms (18.20%) | Memory: 29.7 MB (11.13%) - LeetSync

---
+++ 652-find-duplicate-subtrees/find-duplicate-subtrees.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def walkTree(self, root: Optional[TreeNode], callback: Any):
+        if not root:
+            return "#"
+        
+        left_hash = self.walkTree(root.left, callback)
+        right_hash = self.walkTree(root.right, callback)
+        
+        hash = f"{root.val}(${left_hash})(${right_hash})"
+        # hash = (
+        #     root.val,
+        #     left_hash,
+        #     right_hash
+        # )
+        
+        callback(root, hash)
+        return hash
+            
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        duplicates = list()
+        groups = dict()
+        
+        def addNodeToDict(node, hash):
+
+            group = groups.setdefault(hash, set())
+
+            if not group:
+                group.add(node)
+                return
+
+            if len(group) == 1:
+                group.add(node)
+                duplicates.append(node)
+                return
+            
+            
+        self.walkTree(root, addNodeToDict)
+        return duplicates
+        
+        

4e431a5f65fe36253c06dda425377716ead0fe6a
Azat <kaumov.a.r@gmail.com>
2024-03-28 12:58:51 +0300

    Time: 285 ms (95.77%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,14 +1,5 @@
--- Write your PostgreSQL query statement below
-WITH Ranks AS (
-    SELECT
-        score,
-        RANK() OVER (ORDER BY score DESC) as rank
-    FROM Scores
-    GROUP BY score
-)
-SELECT 
-    s.score as score,
-    rank
-FROM Scores AS s
-JOIN Ranks AS r ON s.score = r.score
-ORDER BY s.score DESC
+SELECT
+    score,
+    DENSE_RANK() OVER (ORDER BY score DESC) as rank
+FROM Scores
+ORDER BY score DESC

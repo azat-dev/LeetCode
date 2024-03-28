6e33730c6e089db096e4a807b4e8d6b542ab2f8d
Azat <kaumov.a.r@gmail.com>
2024-03-29 00:07:51 +0300

    Time: 0 ms (100.00%) | Memory: 41.5 MB (27.00%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        
-        int value = 1;
         final var size = n * n;
-
         final var matrix = new int[n][n];
+        final var emptyValue = 0;
 
+        // Setup the first value
+        int value = 1;
+
+        // Setup initial index
         var rowIndex = 0;
         var colIndex = 0;
 
+        // Setup initial direction (from bottom to right)
         var rowStep = 0;
         var colStep = 1;
 
@@ -20,27 +23,36 @@ class Solution {
             final var nextRowIndex = rowIndex + rowStep;
             final var nextColIndex = colIndex + colStep;
 
-            if (
+            final var isNextCellInsideOfBoundaries = (
                 nextRowIndex < n && 
                 nextColIndex < n && 
                 nextRowIndex >= 0 &&
-                nextColIndex >= 0 &&
-                matrix[nextRowIndex][nextColIndex] == 0
+                nextColIndex >= 0
+            );
+
+            if (
+                isNextCellInsideOfBoundaries &&
+                matrix[nextRowIndex][nextColIndex] == emptyValue
             ) {
+                // The next cell is inside of the boundaries
                 rowIndex = nextRowIndex;
                 colIndex = nextColIndex;
                 continue;
             }
 
+            // The next cell is outside of boundaries
             // Change direction
             if (colStep != 0) {
+                // Use vertical direction now
                 rowStep = colStep > 0 ? 1 : -1;
                 colStep = 0;
             } else {
+                // Use horizontal direction now
                 colStep = rowStep > 0 ? -1 : 1;
                 rowStep = 0;
             }
 
+            // Move to the next cell
             rowIndex += rowStep;
             colIndex += colStep;
         }

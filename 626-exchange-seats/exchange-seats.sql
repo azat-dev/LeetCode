3ed0af8dbe6ebc0a709943a88b3d76b519087965
Azat <kaumov.a.r@gmail.com>
2024-04-01 22:56:10 +0300

    Time: 1241 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,17 +1,11 @@
 # Write your MySQL query statement below
 SELECT
-    id,
+    Seat.id as id,
     CASE
-        WHEN is_even THEN prev_student
-        WHEN next_student IS NULL THEN student
-        ELSE next_student
+        WHEN Seat.id % 2 = 0 THEN PrevSeat.student
+        WHEN NextSeat.id IS NULL THEN Seat.student
+        ELSE NextSeat.student
     END as student
-FROM (
-    SELECT
-        id,
-        student,
-        LAG(student, 1) OVER (ORDER BY id) as prev_student,
-        LEAD(student, 1) OVER (ORDER BY id) as next_student,
-        (RANK() OVER (ORDER BY id)) % 2 = 0 as is_even
-    FROM Seat
-) AS SeatsWithAdditionalInfo
+FROM Seat
+    LEFT JOIN Seat as NextSeat ON Seat.id = NextSeat.id - 1
+    LEFT JOIN Seat as PrevSeat ON Seat.id = PrevSeat.id + 1

cac21334f8061327a33416e2eac838873b0920f4
Azat <kaumov.a.r@gmail.com>
2024-04-07 00:15:04 +0300

    Time: 27 ms (5.72%) | Memory: 44.8 MB (8.55%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public boolean checkSubtree(TreeNode root, TreeNode subRoot) {
+        
+        if (subRoot == null && root == null) {
+            return true;
+        }
+
+        if (root == null || subRoot == null) {
+            System.out.println("False2");
+            return false;
+        }
+
+        if (root.val != subRoot.val) {
+            System.out.println("False1");
+            return false;
+        }
+
+        return checkSubtree(root.left, subRoot.left) && checkSubtree(root.right, subRoot.right);
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        
+        if (root == null) {
+            return false;
+        }
+        
+        // Check current node
+        if (checkSubtree(root, subRoot)) {
+            return true;
+        }
+
+        // Anyway let's try to check next nodes
+        return isSubtree(root.left, subRoot) || 
+            isSubtree(root.right, subRoot);
+    }
+}

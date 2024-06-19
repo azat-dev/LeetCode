09bb75860d547906e0b1be0e9c521d445073b24b
Azat <kaumov.a.r@gmail.com>
2024-06-19 13:01:47 +0300

    Time: 11 ms (8.78%) | Memory: 45 MB (9.77%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.java
+++ 2159-two-out-of-three/two-out-of-three.java
@@ -1,19 +1,28 @@
 class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
         
-        final var s1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
-        final var s2 = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
-        final var s3 = Arrays.stream(nums3).boxed().collect(Collectors.toSet());
+        // Преобразование массивов в множества
+        Set<Integer> s1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        Set<Integer> s2 = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+        Set<Integer> s3 = Arrays.stream(nums3).boxed().collect(Collectors.toSet());
 
-        final var items12 = new HashSet(s1);
+        // Создание копии множеств для нахождения пересечений
+        Set<Integer> items12 = new HashSet<>(s1);
+        
+        // Нахождение пересечений s1 и s2
         items12.retainAll(s2);
 
+        // Нахождение пересечений s1 и s3
         s1.retainAll(s3);
+
+        // Нахождение пересечений s2 и s3
         s2.retainAll(s3);
 
+        // Объединение всех найденных пересечений в одно множество
         items12.addAll(s1);
         items12.addAll(s2);
 
+        // Преобразование результата в список и возврат
         return new ArrayList<>(items12);
     }
 }
